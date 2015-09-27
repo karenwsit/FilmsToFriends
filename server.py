9b7233791548d4f92600da292d42c958909c1dac
Antonia Wang <antonia.t.wang@gmail.com>
2015-09-27 16:35:44 -0700

    logo added

--- server.py
+++ server.py
@@ -177,13 +177,13 @@ def commit_personality_load_movies():
                                     )).all()
 
     print "by_genre is", by_genre
-    return render_template("results.html", session=session)
+    return render_template("results.html", session=session, movies=by_genre)
 
 ###############################
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
     # that we invoke the DebugToolbarExtension
-    app.debug = True
+    app.debug = False
 
     connect_to_db(app)
 
